3b1624fa9b06af5b72ed4ac4a169f50b38a35f3e
IpsitaDas1510 <ipsita.das1510@gmail.com>
2025-12-12 08:25:29 +0000

    added navbar

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -4,7 +4,7 @@ export default function Footer() {
   return (
     <footer className="bg-white mt-12 py-4 border-t">
       <p className="text-center text-gray-500 text-sm">
-        © {new Date().getFullYear()} Crafted By Ipsita <Das></Das>
+        © {new Date().getFullYear()} Crafted By Ipsita Das
       </p>
     </footer>
   );
